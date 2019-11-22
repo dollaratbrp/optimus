0efae06a045f61213c4c06edffaab2c09f2a85f5
Rayn2402 <nicolas.raymon2@usherbrooke.ca>
2019-11-22 14:58:27 -0500

    Correction in _warehouse_init function, now uses class attributes

--- LoadBuilding/LoadBuilder.py
+++ LoadBuilding/LoadBuilder.py
@@ -24,7 +24,7 @@ from datetime import date
 class LoadBuilder:
 
     def __init__(self, plant_from, plant_to, models_data, trailers_data, minimum_trailer, maximum_trailer,
-                 shipping_date, overhang_authorized=72, maximum_trailer_length=636, plc_lb=0.75):
+                 shipping_date, overhang_authorized=40, maximum_trailer_length=636, plc_lb=0.75):
 
         """
         :param plant_from: name of the plant from where the item are shipped
@@ -38,14 +38,14 @@ class LoadBuilder:
         :param maximum_trailer_length: maximum length authorized by law for a trailer
         :param plc_lb: lower bound of length percentage covered that must be satisfied for all trailer
         """
-        self.models_data = models_data
-        self.trailers_data = trailers_data
+        self.models_data_pointer = models_data
+        self.trailers_data_pointer = trailers_data
         self.overhang_authorized = overhang_authorized  # In inches
         self.max_trailer_length = maximum_trailer_length  # In inches
         self.plc_lb = plc_lb
         self.plant_from = plant_from
         self.plant_to = plant_to
-        self.model_names, self.warehouse, self.remaining_crates = self.__warehouse_init(models_data)
+        self.model_names, self.warehouse, self.remaining_crates = [], LoadObj.Warehouse(), LoadObj.CratesManager()
         self.trailers = self.__trailers_init(trailers_data, overhang_authorized, maximum_trailer_length)
         self.minimum_trailer = minimum_trailer
         self.maximum_trailer = maximum_trailer
@@ -53,41 +53,32 @@ class LoadBuilder:
         self.unused_models = []
         self.second_phase_activated = False
 
-    @staticmethod
-    def __warehouse_init(models_data):
+    def __warehouse_init(self):
 
         """
         Initializes a warehouse according to the models available in model data
 
-        :param models_data: Pandas data frame containing details on models to load
-        :return: List of all the model names, Warehouse with the stacks created and a CratesManager with leftover crates
         """
-        # Creation of a warehouse and a crate manager that will be returned by the function
-        warehouse = LoadObj.Warehouse()
-        remaining_crates = LoadObj.CratesManager()
-
-        # Creation of a list that will contain model names that will also be returned
-        model_names = []
 
         # For all lines of the data frame
-        for i in models_data.index:
+        for i in self.models_data.index:
 
             # We save the quantity of the model
-            qty = models_data['QTY'][i]
+            qty = self.models_data['QTY'][i]
 
             if qty > 0:
 
                 # We save the name of the model
-                model_names.append(models_data['MODEL'][i])
+                self.model_names.append(self.models_data['MODEL'][i])
 
                 # We save the stack limit
-                stack_limit = models_data['STACK_LIMIT'][i]
+                stack_limit = self.models_data['STACK_LIMIT'][i]
 
                 # We save the number of models per crate
-                nbr_per_crate = models_data['NBR_PER_CRATE'][i]
+                nbr_per_crate = self.models_data['NBR_PER_CRATE'][i]
 
                 # We save the overhang permission indicator
-                overhang = bool(models_data['OVERHANG'][i])
+                overhang = bool(self.models_data['OVERHANG'][i])
 
                 # We compute the number of models per stack
                 items_per_stack = stack_limit * nbr_per_crate
@@ -97,10 +88,10 @@ class LoadBuilder:
 
                 for j in range(nbr_stacks):
                     # We build the stack and send it into the warehouse
-                    warehouse.add_stack(LoadObj.Stack(max(models_data['LENGTH'][i], models_data['WIDTH'][i]),
-                                                      min(models_data['WIDTH'][i], models_data['LENGTH'][i]),
-                                                      models_data['HEIGHT'][i] * stack_limit,
-                                                      [models_data['MODEL'][i]] * items_per_stack, overhang))
+                    self.warehouse.add_stack(LoadObj.Stack(max(self.models_data['LENGTH'][i], self.models_data['WIDTH'][i]),
+                                                      min(self.models_data['WIDTH'][i], self.models_data['LENGTH'][i]),
+                                                      self.models_data['HEIGHT'][i] * stack_limit,
+                                                      [self.models_data['MODEL'][i]] * items_per_stack, overhang))
 
                 # We save the number of individual crates to build and convert it into
                 # integer to avoid conflict with range function
@@ -108,14 +99,13 @@ class LoadBuilder:
 
                 for j in range(nbr_individual_crates):
                     # We build the crate and send it to the crates manager
-                    remaining_crates.add_crate(LoadObj.Crate([models_data['MODEL'][i]] * nbr_per_crate,
-                                                             max(models_data['LENGTH'][i],
-                                                             models_data['WIDTH'][i]),
-                                                             min(models_data['WIDTH'][i],
-                                                             models_data['LENGTH'][i]),
-                                                             models_data['HEIGHT'][i], stack_limit, overhang))
-
-        return model_names, warehouse, remaining_crates
+                    self.remaining_crates.add_crate(LoadObj.Crate([self.models_data['MODEL'][i]] * nbr_per_crate,
+                                                                  max(self.models_data['LENGTH'][i],
+                                                                      self.models_data['WIDTH'][i]),
+                                                                  min(self.models_data['WIDTH'][i],
+                                                                      self.models_data['LENGTH'][i]),
+                                                                  self.models_data['HEIGHT'][i],
+                                                                  stack_limit, overhang))
 
     @staticmethod
     def __trailers_init(trailers_data, overhang_authorized, maximum_trailer_length):
