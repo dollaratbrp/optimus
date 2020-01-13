7f08f214a5e79bae5206989ac5eb010eca0957bc
rayn2402 <Nicolas.Raymond@brp.com>
2020-01-13 13:29:16 -0500

    Simplified GUI look with trailer data entries added

--- Load_Process/FastLoads.py
+++ Load_Process/FastLoads.py
@@ -8,6 +8,8 @@ import pandas as pd
 import numpy as np
 from openpyxl import load_workbook
 from Import_Functions import SQLConnection
+from P2P_Functions import get_trailers_data
+from LoadBuilder import LoadBuilder
 
 workbook_path = 'U:\LoadAutomation\Optimus\FastLoadsSKUs.xlsx'
 
@@ -20,36 +22,55 @@ class FastLoadsBox:
         Generates a GUI that allows user to valid fast load details and to start the execution
         :param master: root of the tkinter window
         """
+
+        # Saving of master and setting of GUI title
         self.master = master
         self.master.title('Optimus FastLoads')
 
-        # Initialization and positioning of frame that will contain sku labels
-        self.frame = Frame(self.master, borderwidth=2, relief=RIDGE)
-        self.frame.grid(row=0, column=0)
-
-        # "SKU" and "QTY" labels initialization and positioning
-        self.sku_label = Label(self.frame, text='SKU', padx=50, pady=10, borderwidth=2, relief=RIDGE)
-        self.qty_label = Label(self.frame, text='QTY', padx=50, pady=10, borderwidth=2, relief=RIDGE)
-        self.sku_label.grid(row=0, column=0)
-        self.qty_label.grid(row=0, column=1)
+        # Initialization and positioning of a frame that will contain sku labels and entries
+        self.sku_frame = LabelFrame(self.master, borderwidth=2, relief=RIDGE, text='Crates')
+        self.sku_frame.grid(row=0, column=0)
 
         # All single sku labels and qty entries initialization and positioning
-        self.labels, self.entries = self.create_labels_and_entries(self.frame, self.read_skus_and_quantities())
+        self.sku_labels, self.sku_entries = self.create_sku_labels_and_entries(self.sku_frame,
+                                                                               self.read_skus_and_quantities())
+        # Recuperation of trailers data
+        self.trailers_data = get_trailers_data()
+
+        # Initialization and positioning of a frame that will contain trailers labels and entries
+        self.trailer_frame = LabelFrame(self.master, borderwidth=2, relief=RIDGE, text='Trailers')
+        self.trailer_frame.grid(row=0, column=1)
+
+        # All single trailer labels and qty entries initialization and positioning
+        self.trailer_labels, self.trailer_qty_entries = self.create_trailer_labels_and_entries(self.trailer_frame)
+
+        # Initialization and positioning of a frame for max loads entry
+        self.max_frame = Frame(self.trailer_frame, borderwidth=2, relief=RIDGE)
+        self.max_frame.grid(row=len(self.trailer_labels)+1, columnspan=2)
+        self.max_label = Label(self.max_frame, text="MAX LOADS", padx=4, pady=10)
+        self.max_entry = Entry(self.max_frame, justify='center', width=10)
+        self.max_entry.insert(0, "∞")
+        self.max_label.grid(row=0, column=0)
+        self.max_entry.grid(row=0, column=1)
 
         # "Run optimus" button configurations
-        self.run_button = Button(self.master, text='Run Optimus', padx=50, pady=10, command=self.run_optimus)
-        self.run_button.grid(row=1, columnspan=2, sticky=E+W)
+        self.run_button = Button(self.master, text='Run Optimus', padx=50, pady=10, command=self.run_optimus, bd=3,
+                                 font=('TkDefaultFont', 12, 'italic'), bg='gray80')
+        self.run_button.grid(row=2, columnspan=2, sticky=E+W)
+
+        # Initialization of an empty load builder attribute
+        self.LoadBuilder = None
 
     @staticmethod
-    def create_labels_and_entries(frame, skus_n_qty):
+    def create_sku_labels_and_entries(frame, skus_n_qty):
         """
-        Creates and places wisely sku labels and qty entries in our frame
+        Creates and places wisely sku labels and qty entries in the frame entered as parameter
         :param frame: frame widget of our FastLoadsBox
         :param skus_n_qty: pandas dataframe containing SKUs and quantities associated to each
         :return: list of labels and list of entries
         """
 
-        # Labels and entries container
+        # Labels and entries container initialization
         labels = []
         entries = []
 
@@ -60,9 +81,33 @@ class FastLoadsBox:
             labels[i].grid(row=i+1, column=0)
 
             # Qty entries initialization and positioning
-            entries.append(Entry(frame, justify='center'))
+            entries.append(Entry(frame, justify='center', width=10))
             entries[i].insert(0, str(skus_n_qty['QTY'][i]))
             entries[i].grid(row=i+1, column=1)
+
+        return labels, entries
+
+    def create_trailer_labels_and_entries(self, frame):
+        """
+        Creates and places wisely trailer labels and qty entries in the frame entered as parameter
+        :param frame: frame widget of our FastLoadsBox
+        :return: list of labels and list of entries
+        """
+
+        # Labels and entries container initialization
+        labels = []
+        entries = []
+
+        for i in self.trailers_data.index:
+
+            # Trailer label initialization and positioning
+            labels.append(Label(frame, text=str(self.trailers_data['CATEGORY'][i]), padx=10, pady=10))
+            labels[i].grid(row=i + 1, column=0)
+
+            # Qty entries initialization and positioning
+            entries.append(Entry(frame, justify='center', width=10))
+            entries[i].insert(0, 0)
+            entries[i].grid(row=i + 1, column=1)
 
         return labels, entries
 
@@ -89,11 +134,31 @@ class FastLoadsBox:
         Run Optimus program on our selection of SKUs
         :return:
         """
+        # We save definitive quantities of SKUs and trailers
         skus_list, qty_list = self.save_skus_and_quantities()
+        number_of_trailers = self.save_trailers_quantities()
 
-        if len(skus_list) != 0:
+        if len(skus_list) != 0 and number_of_trailers != 0:
+
+            # Building of dataframes required for the loading
+            # (df1) Keeps track of link between SKUs and size code
+            # (df2) Dataframe needed by the load builder
             complete_dataframe = self.get_complete_dataframe(skus_list, qty_list)
             df1, df2 = self.split_dataframes(complete_dataframe)
+
+            # Initialization of our LoadBuilder
+            self.LoadBuilder = LoadBuilder(trailers_data=self.trailers_data)
+
+            # Recuperation of the maximum of loads
+            max_loads = self.max_entry.get()
+            if max_loads == '∞':
+                max_loads = 5000
+            else:
+                max_loads = int(max_loads)
+
+            # Closing of the GUI and construction of loads
+            self.master.destroy()
+            size_code_used = self.LoadBuilder.build(models_data=df2, max_load=max_loads, plot_load_done=True)
 
         pass
 
@@ -107,13 +172,40 @@ class FastLoadsBox:
         qty_list = []   # To use as column in pandas dataframe
 
         # We save each sku in a list if their quantity is greater than 0
-        for i in range(len(self.labels)):
-            qty = int(self.entries[i].get())
+        for i in range(len(self.sku_labels)):
+            qty = int(self.sku_entries[i].get())
             if qty > 0:
-                skus_list.append(self.labels[i]['text'])
+                skus_list.append(self.sku_labels[i]['text'])
                 qty_list.append(qty)
 
         return skus_list, qty_list
+
+    def save_trailers_quantities(self):
+        """
+        Save definitive quantities of trailers in our trailer dataframe
+
+        :return : total number of trailers
+        """
+
+        # Initialization of a counter of trailers
+        number_of_trailers = 0
+
+        for i in self.trailers_data.index:
+            qty = int(self.trailer_qty_entries[i].get())
+            self.trailers_data.at[i, 'QTY'] = qty
+            number_of_trailers += qty
+
+        return number_of_trailers
+
+    @staticmethod
+    def positive(a):
+        """
+        Look if the input is positive, we don't check if it's an integer because it's automatically converted as an int
+        in save_skus_and_quantitites and save_trailer_quantities.
+        :param a: variable
+        :return: raise an Exception if the input is not positive nor integer
+        """
+        raise NotImplementedError
 
     @staticmethod
     def get_complete_dataframe(skus_list, qty_list):
